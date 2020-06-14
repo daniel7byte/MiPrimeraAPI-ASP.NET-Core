01aae6fbe931947711315d202944d408a3e45dec
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2020-06-13 21:31:12 -0500

    first commit

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -8,26 +8,37 @@ using MiPrimeraAPI.Interfaces;
 
 namespace MiPrimeraAPI.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CoursesController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class CoursesController : ControllerBase
+  {
+    private readonly ICoursesProvider coursesProvider;
+
+    public CoursesController(ICoursesProvider coursesProvider)
     {
-        private readonly ICoursesProvider coursesProvider;
-
-        public CoursesController(ICoursesProvider coursesProvider)
-        {
-            this.coursesProvider = coursesProvider;
-        }
-
-        //[HttpGet]
-        [HttpGet("{number}")]
-        public IActionResult Get(int number) // ActionResult<int>
-        {
-            if(number < 100)
-            {
-                return Ok(number);
-            }
-            return NotFound(number);
-        }
+      this.coursesProvider = coursesProvider;
     }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync()
+    {
+      var results = await coursesProvider.GetAllAsync();
+      if (results != null)
+      {
+        return Ok(results);
+      }
+
+      return NotFound();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetAsync(int id)
+    {
+      var result = await coursesProvider.GetAsync(id);
+      if (result != null)
+      {
+
+      }
+    }
+  }
 }
